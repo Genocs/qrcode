cff9b3585c6556aeb5b607a0b3b5940e803dc5d9
Giovanni Emanuele Nocco <giovanni.nocco@gmail.com>
2020-04-10 21:11:58 +0200

    ganged dockerizer

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -13,7 +13,7 @@ case "$TRAVIS_BRANCH" in
     ;;    
 esac
 
-REPOSITORY=$DOCKER_USERNAME/genocs.qrcode
+REPOSITORY=$DOCKER_CONTAINER/genocs.qrcode
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -t $REPOSITORY:$TAG -t $REPOSITORY:$VERSION_TAG .
