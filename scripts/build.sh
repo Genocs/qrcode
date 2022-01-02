1edaed6fce55adb1dca26d69bb423021bde9ff7e
Giovanni Emanuele Nocco <giovanni.nocco@gmail.com>
2022-01-02 19:32:49 +0100

    aligned services

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+MYGET_ENV=""
+case "$TRAVIS_BRANCH" in
+  "develop")
+    MYGET_ENV="-dev"
+    ;;
+esac
+
 dotnet build -c release
--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -5,17 +5,17 @@ VERSION_TAG=
 case "$TRAVIS_BRANCH" in
   "master")
     TAG=latest
-    VERSION_TAG=$TRAVIS_BUILD_NUMBER
+    VERSION_TAG=1.0.$TRAVIS_BUILD_NUMBER
     ;;
   "develop")
     TAG=dev
     VERSION_TAG=$TAG-$TRAVIS_BUILD_NUMBER
-    ;;    
+    ;;
 esac
 
 REPOSITORY=$DOCKER_CONTAINER/qrcode
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -t $REPOSITORY:$TAG -t $REPOSITORY:$VERSION_TAG .
+docker build -t $REPOSITORY:$TAG -t $REPOSITORY:$VERSION_TAG -f ./src/Genocs.QRCodeLibrary.WebApi/Dockerfile .
 docker push $REPOSITORY:$TAG
 docker push $REPOSITORY:1.0.$VERSION_TAG
